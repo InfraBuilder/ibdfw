dff3830037f6ae968079d63b7808a9c0fb2a5639
Alexis Ducastel <alexis@infrabuilder.com>
2020-01-03 02:52:22 +0100

    removing iptables command from modules, reorg code

--- build-local.sh
+++ build-local.sh
@@ -1,2 +1,5 @@
+VER=$(( $(grep "^version:" snapcraft.yaml| grep -o "[1-9][0-9]*") + 1 ))
+echo "Building version $VER"
+sed -i "s/version: .*/version: '0.0.$VER'/" snapcraft.yaml
 snapcraft clean ibdfw -s pull
 snapcraft
