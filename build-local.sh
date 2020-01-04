d4e559923b21b230caffe90dfa8cd26abb679e76
Alexis Ducastel <alexis@infrabuilder.com>
2020-01-04 02:02:50 +0100

    Version 0.1.1 : Default, Output, Input, Forward, Raw, Nat are functionnal

--- build-local.sh
+++ build-local.sh
@@ -1,6 +1,8 @@
-VER=$(( $(grep "^version:" snapcraft.yaml| grep -o "[1-9][0-9]*") + 1 ))
-echo "Building version $VER"
-sed -i "s/version: .*/version: '0.0.$VER'/" snapcraft.yaml
+#!/bin/bash
+VERSION=$(grep "^version:" snapcraft.yaml| grep -o "[0-9]*\.[0-9]*\.[0-9]*")
+NEXT="$(echo $VERSION| awk -F. '{print $1"."$2}')".$(( $(echo $VERSION| awk -F. '{print $3}') + 1 ))
+echo "Building version $NEXT"
+sed -i "s/version: .*/version: '$NEXT'/" snapcraft.yaml
 #snapcraft clean ibdfw -s pull
 snapcraft
 rm -rf parts/ prime/ stage/
--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 
 snapfile=$(ls -1 ibdfw_*_amd64.snap|sort -V|tail -n 1)
 sudo snap install --classic --dangerous $snapfile
+#sudo snap install --dangerous $snapfile
