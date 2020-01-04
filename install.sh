7482aa9a7e80e6e11035982d2fa0e25bd7081fab
Alexis Ducastel <alexis@infrabuilder.com>
2020-01-04 19:47:59 +0100

    Adapted for Snap strict confinement : breaking conf directory

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 snapfile=$(ls -1 ibdfw_*_amd64.snap|sort -V|tail -n 1)
-sudo snap install --classic --dangerous $snapfile
-#sudo snap install --dangerous $snapfile
+sudo snap install --dangerous $snapfile
